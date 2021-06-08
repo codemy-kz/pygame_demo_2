6eeadde7e3ffd21fa4aad4f595eb625610706cbb
Daulet Baimbetov <daucloudlab@gmail.com>
2021-06-08 17:11:40 +0600

    Бірнеше суреттер қосылды және суреттер айнала қозғалады

---
+++ test6.py
@@ -0,0 +1,169 @@
+import pygame
+import random
+from os import path
+
+img_dir = path.join(path.dirname(__file__), "img")
+
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(player_img, (50,38))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH // 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+        self.radius = 20
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 5
+
+        self.rect.x += self.speedx
+
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+    
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image_orig = random.choice(meteor_images)
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, WIDTH-self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+        self.radius = int(self.rect.width*0.85 / 2)
+        self.rot = 0
+        self.rot_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()
+
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pygame.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
+
+    def update(self):
+        self.rotate()
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(0, WIDTH-self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+    
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx  = x
+        self.rect.bottom = y
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill() 
+
+# pygame-ді инициализациялау және терезені құру
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Action Game Demo")
+clock = pygame.time.Clock()
+
+background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
+background_pos = background.get_rect()
+player_img = pygame.image.load(path.join(img_dir, 'playerShip1_orange.png')).convert()
+# meteor_img = pygame.image.load(path.join(img_dir, 'meteorBrown_med1.png')).convert()
+meteor_images = []
+meteor_list = ['meteorBrown_big1.png', 'meteorBrown_med1.png', 'meteorBrown_med1.png',
+               'meteorBrown_med3.png', 'meteorBrown_small1.png', 'meteorBrown_small2.png',
+               'meteorBrown_tiny1.png']
+for img in meteor_list:
+    meteor_images.append(pygame.image.load(path.join(img_dir, img)).convert())
+
+bullet_img = pygame.image.load(path.join(img_dir, 'laserRed16.png')).convert()
+
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+mobs = pygame.sprite.Group()
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+bullets = pygame.sprite.Group()
+
+# Game loop
+flRunning = True
+while flRunning:
+    clock.tick(FPS)
+    
+    # Process inputs(events)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            flRunning = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+            
+    # Update
+    all_sprites.update()
+    
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for h in hits:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
+
+    hit = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
+    if hit:
+        flRunning = False
+        
+    # Draw / render
+    screen.blit(background, background_pos)
+    all_sprites.draw(screen)
+    
+    # draw -дан кейін
+    pygame.display.update()
+
+pygame.quit()
