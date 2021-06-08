68771bf03b6d455fa6162ffdbce0d593e2003ca7
Daulet Baimbetov <daucloudlab@gmail.com>
2021-06-08 08:21:00 +0600

    Sprite Demo

--- test1.py
+++ test1.py
@@ -1,9 +1,10 @@
 import pygame
 import random
+import os
 
 WIDTH = 800
 HEIGHT = 600
-FPS = 60
+FPS = 30
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -11,18 +12,29 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+# assets папканың қайда екенің анықтайды
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img") 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(GREEN)
+        self.image = pygame.image.load(os.path.join(img_folder, "p1_jump.png")).convert_alpha()
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH // 2, HEIGHT // 2)
+        self.y_speed = 5
     
     def update(self):
         self.rect.x += 5
         if self.rect.left > WIDTH:
             self.rect.right = 0
+
+        self.rect.y += self.y_speed
+        if self.rect.bottom > HEIGHT - 200:
+            self.y_speed = -5
+        if self.rect.top < 200:
+            self.y_speed = 5
 
 # pygame-ді инициализациялау және терезені құру
 pygame.init()
@@ -49,7 +61,7 @@ while flRunning:
     all_sprites.update()
 
     # Draw / render
-    screen.fill(BLACK)
+    screen.fill(BLUE)
     all_sprites.draw(screen)
     
     # draw -дан кейін
