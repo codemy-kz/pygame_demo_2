78d286cad7c60d1dd1fc47d9a14e3d0e481f567c
Daulet Baimbetov <daucloudlab@gmail.com>
2021-06-08 09:42:17 +0600

    Action стиліндегі ойынды бастадым

---
+++ test2.py
@@ -0,0 +1,55 @@
+import pygame
+import random
+
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH // 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.rect.x += self.speedx
+
+# pygame-ді инициализациялау және терезені құру
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Action Game Demo")
+clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+# Game loop
+flRunning = True
+while flRunning:
+    clock.tick(FPS)
+    
+    # Process inputs(events)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            flRunning = False
+            pygame.quit()
+    # Update
+    all_sprites.update()
+
+    # Draw / render
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    
+    # draw -дан кейін
+    pygame.display.update()
